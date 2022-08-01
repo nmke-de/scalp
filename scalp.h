5ee51273886c995247421203caaa715f5b9fcac8
nmke-de <github@nmke.de>
2022-08-01 17:35:54 +0200

    Added initial version of scalp.h

---
+++ scalp.h
@@ -0,0 +1,13 @@
+#ifndef SCALP_H
+#define SCALP_H
+#include <time.h>
+
+typedef struct {
+	time_t when;
+	char *text;
+} event;
+
+void notify_send(const char *);
+void read_file(event *, const char *); // TODO change signature before use
+
+#endif
