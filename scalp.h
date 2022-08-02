2a9d9d30d965ccccdfaf81ad5295aba7e1733254
nmke-de <github@nmke.de>
2022-08-02 09:05:08 +0200

    Added read_file() implementation

--- scalp.h
+++ scalp.h
@@ -2,12 +2,14 @@
 #define SCALP_H
 #include <time.h>
 
+#define MAX_EVENT_TEXT 512
+
 typedef struct {
 	time_t when;
 	char *text;
 } event;
 
 void notify_send(char *);
-event *read_file(const char *); // TODO change signature before use
+event *read_file(const char *, int *); // TODO change signature before use
 
 #endif
