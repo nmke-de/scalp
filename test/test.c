96f4d0fe6e426268da280bc66cf3ab0e9b62235e
nmke-de <github@nmke.de>
2022-08-03 09:14:38 +0200

    Added new (imcomplete) test

--- test/test.c
+++ test/test.c
@@ -13,8 +13,13 @@ void test_read_file_1() {
 	free(ev);
 }
 
+void test_notify_send_1() {
+	notify_send("Test.");
+}
+
 int main() {
 	test_read_file_1();
+	test_notify_send_1();
 	char *msg = "All tests successful.\n";
 	write(0, msg, strlen(msg));
 	return 0;
