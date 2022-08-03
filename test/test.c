e39749f40776fed4a037b5f0dce1d7d6667fb70f
nmke-de <github@nmke.de>
2022-08-03 09:31:23 +0200

    Added another test

--- test/test.c
+++ test/test.c
@@ -13,12 +13,24 @@ void test_read_file_1() {
 	free(ev);
 }
 
+void test_read_file_2() {
+	int size;
+	event *ev = read_file("test2.scalp", &size);
+	assert(size == 2);
+	assert(ev[0].when == 0);
+	assert(strcmp(ev[0].text, "Hahatop") == 0);
+	assert(ev[1].when == 1659511309);
+	assert(strcmp(ev[1].text, "asda") == 0);
+	free(ev);
+}
+
 void test_notify_send_1() {
 	notify_send("Test.");
 }
 
 int main() {
 	test_read_file_1();
+	test_read_file_2();
 	test_notify_send_1();
 	char *msg = "All tests successful.\n";
 	write(0, msg, strlen(msg));
