484633adddc21566e2d3c8e76a37696492582279
nmke-de <github@nmke.de>
2022-08-03 12:14:47 +0200

    Removed commented lines

--- scalp.c
+++ scalp.c
@@ -6,11 +6,7 @@ int main() {
 	time_t t = time(NULL);
 	int size;
 	event *ev = read_file("foo", &size); // TODO change file name
-	//event one_minute_from_now;
-	//one_minute_from_now.when = t + 60;
-	//one_minute_from_now.text = "One Minute has passed.";
 	for(;; t = time(NULL)) {
-		//if(t == one_minute_from_now.when) notify_send(one_minute_from_now.text);
 		for(int i = 0; i < size; i++) if(t == ev[i].when) notify_send(ev[i].text);
 		sleep(1);
 	}
