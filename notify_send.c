1b0702151564634a262c56ef28e08a413978c65f
nmke-de <github@nmke.de>
2022-08-12 11:48:39 +0200

    Removed a byte from notify_send.c

--- notify_send.c
+++ notify_send.c
@@ -2,6 +2,6 @@
 #include "scalp.h"
 
 void notify_send(char *msg) {
-	char *args[4] = {"notify-send", "BINGO!", msg, NULL};
+	char *args[] = {"notify-send", "BINGO!", msg, NULL};
 	if(fork() == 0) execvp("notify-send", args);
 }
