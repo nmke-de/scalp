88e485b7158d57c8691969dead10e3206bda393a
nmke-de <github@nmke.de>
2022-08-16 20:14:10 +0200

    Using global evlist now (prep issue #7)

--- scalp.c
+++ scalp.c
@@ -3,12 +3,11 @@
 #include "scalp.h"
 
 int main(int argc, char **argv) {
-	char *filename = argc < 2 ? "foo" : argv[1];
+	evlist.filename = argc < 2 ? "foo" : argv[1];
 	time_t t = time(NULL);
-	int size;
-	event *ev = read_file(filename, NULL, &size);
+	evlist.ev = read_file(evlist.filename, NULL, &(evlist.size));
 	for(;; t = time(NULL)) {
-		for(int i = 0; i < size; i++) if(t == ev[i].when) notify_send(ev[i].text);
+		for(int i = 0; i < evlist.size; i++) if(t == evlist.ev[i].when) notify_send(evlist.ev[i].text);
 		sleep(1);
 	}
 	// call to free is optimized away anyway.
