ed364c0c84f9c15cc17beee66d3a2539d63fe020
nmke-de <github@nmke.de>
2022-08-23 23:12:03 +0200

    Refactored argument parsing for better scaling

--- scalp.c
+++ scalp.c
@@ -1,7 +1,17 @@
+#include <string.h>
 #include "scalp.h"
 
+void list (char *dummy) {}
+
 int main(int argc, char **argv) {
-	char *filename = argc < 2 ? "foo" : argv[1];
+	char *filename = "foo";
+	void (*mode)(char *) = scalpd;
+	if (argc > 1) {
+		if (strcmp(argv[1], "list") == 0) mode = list;
+		else filename = argv[1];
+	}
+	if (argc > 2 && strcmp(filename, "foo") == 0) filename = argv[2];
 	scalpd(filename);
+	//mode(filename);
 	return 0;
 }
