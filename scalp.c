2852715f00c9c496cbb823744d39863aaa929ab9
nmke-de <github@nmke.de>
2022-08-23 22:43:44 +0200

    Restructured main() and added scalpd() to header file

--- scalp.c
+++ scalp.c
@@ -3,28 +3,8 @@
 // #include <stdlib.h>
 #include "scalp.h"
 
-static struct {
-	event *ev;
-	int size;
-	char *filename;
-} evlist = {
-	NULL, 0, "foo"
-};
-
-static inline void sig_handle(int signum) {
-	evlist.ev = read_file(evlist.filename, evlist.ev, &(evlist.size));
-}
-
 int main(int argc, char **argv) {
-	evlist.filename = argc < 2 ? "foo" : argv[1];
-	time_t t = time(NULL);
-	evlist.ev = read_file(evlist.filename, NULL, &(evlist.size));
-	signal(SIGUSR1, sig_handle);
-	for(;; t = time(NULL)) {
-		for(int i = 0; i < evlist.size; i++) if(t == evlist.ev[i].when) notify_send(evlist.ev[i].text);
-		sleep(1);
-	}
-	// call to free is optimized away anyway.
-	// free(ev);
+	char *filename = argc < 2 ? "foo" : argv[1];
+	scalpd(filename);
 	return 0;
 }
