ea43daa57d2b62d4ad2d9332b405c0663f646245
nmke-de <github@nmke.de>
2022-08-24 17:53:22 +0200

    now sorts entries by time (issue #3)

--- list.c
+++ list.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "scalp.h"
 
 void list(char *filename) {
 	int size;
 	event *ev = read_file(filename, NULL, &size);
+	qsort(ev, size, sizeof(event), timecompare);
 	for (int i = 0; i < size; i++) {
 		// Print each item
 		fputs(ctime(&(ev[i].when)), stdout);
