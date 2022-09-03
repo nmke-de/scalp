f2db318488045d44907399ab9908711521a1da82
nmke-de <github@nmke.de>
2022-09-03 21:29:38 +0200

    Added add() tentatively. Doesn't work as intended yet though. (issue #5)

--- add.c
+++ add.c
@@ -1,5 +1,8 @@
+#include <unistd.h>
 #include "scalp.h"
 
 void add(char *filename) {
-	;
+	char *args[] = {"sh", "./scalp-add.sh", filename, "interactive", NULL};
+	if (fork() == 0)
+		execvp("sh", args);
 }
