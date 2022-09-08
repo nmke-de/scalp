92c4c270e34aa08937440ececffce63920d4252d
nmke-de <github@nmke.de>
2022-09-08 15:11:32 +0200

    Added SIGUSR2 handle for debugging

--- scalpd.c
+++ scalpd.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 #include "scalp.h"
 
 static struct {
@@ -14,11 +15,20 @@ static inline void sig_handle(int signum) {
 	evlist.ev = read_file(evlist.filename, evlist.ev, &(evlist.size));
 }
 
+#define print(str) write(0, (str), strlen((str)))
+static inline void sig2_handle(int signum) {
+	for (int i = 0; i < evlist.size; i++) {
+		print(evlist.ev[i].text);
+		print("\n");
+	}
+}
+
 void scalpd(char *filename) {
 	time_t t = time(NULL);
 	evlist.filename = filename;
 	evlist.ev = read_file(evlist.filename, NULL, &(evlist.size));
 	signal(SIGUSR1, sig_handle);
+	signal(SIGUSR2, sig2_handle);
 	for (;; t = time(NULL)) {
 		for (int i = 0; i < evlist.size; i++)
 			if (t == evlist.ev[i].when)
