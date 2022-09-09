d09597fb226c90bcc740d1573e58f87a0e7badd0
nmke-de <github@nmke.de>
2022-09-09 09:26:10 +0200

    Implemented add() (issue #5)

--- add.c
+++ add.c
@@ -8,34 +8,39 @@
 
 void add(char *filename) {
 	char msg[512] = "TESTIFICATE";
-	char tmp_time[64] = "today 4pm";
-	char buf[21];
-	// Spawn child process and open pipe
+	char tmp_time[64] = "";
+	// Read time from stdin. Repeat if format is bad.
+	char buf[21] = "";
 	int pfd[2];
-	if (pipe(pfd) < 0)
-		return;
-	pid_t pid = fork();
-	if (pid < 0) {
-		close(pfd[0]);
-		close(pfd[1]);
-		return;
-	} else if (pid == 0) {
-		char *argv[] = {"date", "+%s", "-d", tmp_time, 0};
-		close(pfd[0]);
-		close(1);
-		dup2(pfd[1], 1);
-		close(pfd[1]);
-		execvp("date", argv);
-	}
-	close(pfd[1]);
-	fcntl(pfd[0], F_SETFD, FD_CLOEXEC);
-	// Read input from *pfd
-	int i = 0;
 	do {
-		read(*pfd, buf + i, 1);
-		++i;
-	} while (buf[i] != 10);
-	close(*pfd);
+		print("Time: ");
+		input(tmp_time, 64);
+		// Spawn child process and open pipe
+		if (pipe(pfd) < 0)
+			return;
+		pid_t pid = fork();
+		if (pid < 0) {
+			close(pfd[0]);
+			close(pfd[1]);
+			return;
+		} else if (pid == 0) {
+			char *argv[] = {"date", "+%s", "-d", tmp_time, 0};
+			close(pfd[0]);
+			close(1);
+			dup2(pfd[1], 1);
+			close(pfd[1]);
+			execvp("date", argv);
+		}
+		close(pfd[1]);
+		fcntl(pfd[0], F_SETFD, FD_CLOEXEC);
+		// Read input from *pfd
+		int i = 0;
+		do {
+			read(*pfd, buf + i, 1);
+			++i;
+		} while (buf[i] != 10);
+		close(*pfd);
+	} while (*buf == 0);
 	// Read message from stdin
 	print("Message: ");
 	int rl = input(msg, 512);
