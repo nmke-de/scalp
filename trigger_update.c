bd60867c4f6614c864b29398cb413b9a190cea3b
nmke-de <github@nmke.de>
2022-09-26 13:16:14 +0200

    Comment

--- trigger_update.c
+++ trigger_update.c
@@ -7,6 +7,7 @@ routine to update all running instances of scalp
 void trigger_update(char *filename) {
 	char *args[] = {"killall", "-s", "USR1", "scalp", NULL};
 	if (fork() == 0) {
+		// Don't annoy me.
 		close(2);
 		execvp("killall", args);
 	}
