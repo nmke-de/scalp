39394eabfa391bce755f75397a96d323873fb5a3
nmke-de <github@nmke.de>
2022-09-29 14:43:52 +0200

    Added append.c

---
+++ append.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "scalp.h"
+
+/*
+append appends a new entry to the file filename with the time time (in Unix epoch format) and the message msg. This function does not check whether the message is too long.
+*/
+int append(char *filename, char *time, int len1, char *msg, int len2) {
+	int fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return;
+	write(fd, time, len1);
+	write(fd, "\t", 1);
+	write(fd, msg, len2);
+	write(fd, "\n", 1);
+	close(fd);
+}
