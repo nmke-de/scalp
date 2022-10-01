63e54eb0dd30b8c5f060b6cdcfed98ccf15d2739
nmke-de <github@nmke.de>
2022-10-01 12:46:31 +0200

    notifications now stick around for 5 minutes

--- notify_send.c
+++ notify_send.c
@@ -5,7 +5,7 @@
 routine to notify the user.
 */
 void notify_send(char *title, char *msg) {
-	char *args[] = {"notify-send", title, msg, NULL};
+	char *args[] = {"notify-send", "-t", "300000", title, msg, NULL};
 	if (fork() == 0)
 		execvp("notify-send", args);
 }
