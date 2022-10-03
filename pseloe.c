754b77b4ddd2bd5c9c7a8ffe7f4215cb9f628c34
nmke-de <github@nmke.de>
2022-10-03 10:12:21 +0200

    Added pseloe.c

---
+++ pseloe.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "scalp.h"
+
+/*
+Print sorted enumerated list of events
+*/
+void pseloe(event *ev, int size) {
+	// Sort entries
+	qsort(ev, size, sizeof(event), timecompare);
+	// Print a list of all entries to output
+	for (int i = 0; i < size; i++) {
+		char *tmp = itoa(i + 1, 10);
+		print(tmp);
+		print("\t");
+		tmp = ctime(&(ev[i].when));
+		write(0, tmp, strlen(tmp) - 1);
+		print("\t");
+		print(ev[i].text);
+		print("\n");
+	}
+}
