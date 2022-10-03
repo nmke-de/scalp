4d55883bc88285f2b26f26236f8771634882f7cd
nmke-de <github@nmke.de>
2022-10-03 09:45:59 +0200

    Added prompt.c

---
+++ prompt.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <string.h>
+#include "scalp.h"
+
+// Macros to make code more managable
+#define print(str) write(0, (str), strlen((str)))
+#define input(var, limit) read(1, (var), (limit))
+
+/*
+Prompts the user with msg to input text in to a buffer buf with bufsize length. Returns the length of input.
+*/
+int prompt(char *buf, int bufsize, char *msg) {
+	// User input
+	print(msg);
+	int rl = input(buf, bufsize);
+	// Check for overflow
+	if (rl == bufsize) {
+		buf[rl - 1] = 0;
+		// Remove characters that are still in the input buffer
+		char junk[16];
+		int rl2;
+		do {
+			rl2 = input(junk, 16);
+		} while (junk[rl2 - 1] != '\n');
+	}
+	return rl;
+}
