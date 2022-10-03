cf98aa1e184ca6488285e73613f53ea980030050
nmke-de <github@nmke.de>
2022-10-03 10:37:35 +0200

    Added change.c

---
+++ change.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "Itoa/itoa.h"
+#include "scalp.h"
+
+void change(char *filename) {
+	int size = 0;
+	char buf[21];
+	char msg[512];
+	// Read file
+	event *ev = read_file(filename, NULL, &size);
+	// Print sorted enumerated list of events
+	pseloe(ev, size);
+	// User input
+	prompt(buf, 21, "Which event do you want to change? ");
+	int change_i = atoi(buf) - 1;
+	// Read in the new time
+	read_time(buf, 21);
+	// Read message from stdin
+	int rl = prompt(msg, 512, "Message: ");
+	// Check for overflow (and add a linefeed)
+	if (rl == 512)
+		msg[rl - 2] = '\n';
+	// Write updates
+	ev[i].when = atol(buf);
+	strncpy(ev[i].text, msg, rl);
+	rm(filename, ev, size, NULL, 0);
+	// Update all running instances
+	trigger_update(filename);
+}
