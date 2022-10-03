8f7c9ca360c1d3d00a59da1636005778308e202a
nmke-de <github@nmke.de>
2022-10-03 10:20:03 +0200

    del() now uses pseloe()

--- del.c
+++ del.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include "Itoa/itoa.h"
 #include "scalp.h"
 
@@ -13,19 +12,8 @@ void del(char *filename) {
 	char buf[21];
 	// Read file
 	event *ev = read_file(filename, NULL, &size);
-	// Sort entries
-	qsort(ev, size, sizeof(event), timecompare);
-	// Print a list of all entries to output
-	for (int i = 0; i < size; i++) {
-		char *tmp = itoa(i + 1, 10);
-		print(tmp);
-		print("\t");
-		tmp = ctime(&(ev[i].when));
-		write(0, tmp, strlen(tmp) - 1);
-		print("\t");
-		print(ev[i].text);
-		print("\n");
-	}
+	// Print sorted enumerated list of events
+	pseloe(ev, size);
 	// User input
 	prompt(buf, 21, "Which event do you want to delete? ");
 	// Index of item that is to be deleted (starting with 1).
