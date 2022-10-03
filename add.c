938f73763f16d1ac7033a6b90fd17f355655b960
nmke-de <github@nmke.de>
2022-10-03 09:53:56 +0200

    add() uses prompt() now

--- add.c
+++ add.c
@@ -17,18 +17,10 @@ void add(char *filename) {
 	char buf[21];
 	read_time(buf, 21);
 	// Read message from stdin
-	print("Message: ");
-	int rl = input(msg, 512);
-	// Check for overflow
-	if (rl == 512) {
-		msg[rl - 1] = 0;
+	int rl = prompt(msg, 512, "Message: ");
+	// Check for overflow (and add a linefeed)
+	if (rl == 512)
 		msg[rl - 2] = '\n';
-		// Remove characters that are still in the input buffer
-		char junk[16];
-		do {
-			rl = input(junk, 16);
-		} while (junk[rl - 1] != '\n');
-	}
 	// Write to file
 	append(filename, buf, strlen(buf) - 1, msg, strlen(msg) - 1);
 	// Update all running instances
