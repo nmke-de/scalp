9c15e083341044ecb821cd594489853b69b48c47
nmke-de <github@nmke.de>
2022-10-08 14:55:06 +0200

    Updated help message

--- help.c
+++ help.c
@@ -10,8 +10,10 @@ Usage: scalp [SUBCOMMAND] [FILENAME]\n\
 Running scalp without subcommand starts the daemon to notify you.\n\
 	add\tadd a new entry\n\
 	del\tdelete an entry\n\
+	change\tchange an entry's message\n\
 	help\tshow this message\n\
 	list\tlist all entries\n\
+	move\tmove an entry (to a new date)\n\
 	prune\tremove all old entries\n\
 	update\tupdate all running instances\n";
 	print(helpmsg);
