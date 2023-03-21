c3a2bdc8621b891636dd97dcd56fc0a7a0c61be9
nmke-de <github@nmke.de>
2023-03-21 22:21:29 +0100

    Changed repeat() implementation

--- repeat.c
+++ repeat.c
@@ -18,16 +18,16 @@ void repeat(char *filename) {
 	prompt(buf, 21, "Which event do you want to repeat? ");
 	int i = atoi(buf) - 1;
 	prompt(buf, 21, "How often? (Y)early, (M)onthly, (W)eekly, (D)aily ");
-	long interval;
+	long interval = 0;
 	char mode = *buf;
-	switch (*buf) {
+	switch (mode) {
 		case 'Y':
 		case 'y':
-			interval = 3600 * 24 * 365;
+			// Solved differently
 			break;
 		case 'M':
 		case 'm':
-			interval = 3600 * 24 * 28;
+			// Solved differently
 			break;
 		case 'W':
 		case 'w':
@@ -47,27 +47,31 @@ void repeat(char *filename) {
 	// Add new events
 	struct tm *t_broken_down = (struct tm *)0;
 	for (time_t t = ev[i].when + interval; t <= stop; t += interval) {
+		// Increment t for year mode and month mode
 		t_broken_down = gmtime(&t);
-		if ((mode == 'Y' || mode == 'y') && isleap(t_broken_down->tm_year))
-			t += (3600 * 24);
-		else if (mode == 'M' || mode == 'm')
-			switch (t_broken_down->tm_mon) {
-				case 0:
-				case 2:
-				case 4:
-				case 6:
-				case 7:
-				case 9:
-				case 11:
-					t += (3600 * 72);
-					break;
-				case 1:
-					if (isleap(t_broken_down->tm_year))
-						t += (3600 * 24);
-					break;
-				default:
-					t += (3600 * 48);
+		t_broken_down->tm_isdst = -1;
+		if ((mode == 'Y' || mode == 'y')) {
+			t_broken_down->tm_year++;
+			// Adding 3600 (1 hour) gives the correct time. I don't know the exact reason for this.
+			t = mktime(t_broken_down) + 3600;
+		} else if (mode == 'M' || mode == 'm') {
+			if (t_broken_down->tm_mon < 11) {
+				t_broken_down->tm_mon++;
+				t = mktime(t_broken_down) + 3600;
+			} else {
+				t_broken_down->tm_year++;
+				t_broken_down->tm_mon = 0;
+				t = mktime(t_broken_down) + 3600;
 			}
+			// Temporary solution to the DST problem. Not exactly correct, but I can't be bothered
+			// with implementing a correct DST solution for scalp at 10pm. I did'nt like DST before, 
+			// but now this is becoming a personal vendetta against DST.
+			if (t_broken_down->tm_mon > 3 && t_broken_down->tm_mon < 11)
+				t += 3600;
+		}
+		// Test for break condition for year mode and month mode
+		if (t > stop)
+			break;
 		append(filename, itoa(t, 10), strlen(itoa(t, 10)), ev[i].text, strlen(ev[i].text));
 	}
 	// Update all running instances
